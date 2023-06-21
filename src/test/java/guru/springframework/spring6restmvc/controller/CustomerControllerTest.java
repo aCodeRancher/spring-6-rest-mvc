42564895931121be68555a9fd40946b80d7c2bf3
Helen Ma <hhma@alumni.cpp.edu>
2023-06-20 20:59:47 -0400

    add save new customer test.

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImpl;
@@ -10,9 +11,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -26,6 +32,8 @@ class CustomerControllerTest {
 
     CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
 
+    @Autowired
+    ObjectMapper objectMapper;
     @Test
     void listAllCustomers() throws Exception {
         given(customerService.getAllCustomers()).willReturn(customerServiceImpl.getAllCustomers());
@@ -48,6 +56,24 @@ class CustomerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.name", is(customer.getName())));
+
+    }
+
+    @Test
+    void saveNewCustomer() throws Exception{
+        Customer addCustomer = Customer.builder().name("Michael Weston")
+                                .id(UUID.randomUUID())
+                                .createdDate(LocalDateTime.now())
+                                .updateDate(LocalDateTime.now())
+                                .version(1).build();
+        String customer =objectMapper.writeValueAsString(addCustomer);
+        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(addCustomer);
+        mockMvc.perform(post("/api/v1/customer")
+                       .content(customer)
+                       .contentType(MediaType.APPLICATION_JSON)
+                       .accept(MediaType.APPLICATION_JSON))
+                       .andExpect(header().exists("Location"))
+                       .andExpect(status().isCreated());
 
     }
 }
