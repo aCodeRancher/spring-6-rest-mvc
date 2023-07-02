c15ef1dee30d087e412397520bf54af8dfcccb84
Helen Ma <hhma@alumni.cpp.edu>
2023-07-02 16:38:28 -0400

    fix exception handler for constraint violation.

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
@@ -19,7 +21,21 @@ public class CustomErrorController {
 
     @ExceptionHandler
     ResponseEntity handleJPAViolations(TransactionSystemException exception){
-        return ResponseEntity.badRequest().build();
+         ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+         if (exception.getCause().getCause() instanceof ConstraintViolationException){
+             ConstraintViolationException ve = (ConstraintViolationException) exception.getCause().getCause();
+             AtomicReference<Map<String, String>> mapAtomicReference = new AtomicReference<>();
+             mapAtomicReference.set(new HashMap<String, String>());
+             List errors = ve.getConstraintViolations().stream()
+                        .map(constraintViolation -> {
+                           //  Map<String, String> errMap = new HashMap<>();
+                             mapAtomicReference.get().put(constraintViolation.getPropertyPath().toString(),
+                                         constraintViolation.getMessage());
+                         return mapAtomicReference.get();
+                     }).collect(Collectors.toList());
+              return responseEntity.body(errors);
+         }
+        return responseEntity.build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
