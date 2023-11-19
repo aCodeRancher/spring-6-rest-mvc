1c74d0be9af5ade7e56142cfbddbd2691fed5230
Helen Ma <hhma@alumni.cpp.edu>
2023-11-19 11:56:15 -0500

    Upgrade the code to use Spring Boot 3.1.5

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,11 +14,9 @@ public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and().oauth2ResourceServer().jwt();
-
-        return http.build();
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+       return http.build();
     }
 
 }
