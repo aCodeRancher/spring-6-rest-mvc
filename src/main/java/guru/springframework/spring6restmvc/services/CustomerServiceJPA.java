d989fb8c09e643ff6eafb3d285026b483fb0ff12
Helen Ma <hhma@alumni.cpp.edu>
2024-06-24 13:00:20 -0400

    add customer cache

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6restmvc.mappers.CustomerMapper;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -25,12 +26,14 @@ public class CustomerServiceJPA implements CustomerService {
     private final CustomerMapper customerMapper;
 
     @Override
+    @Cacheable(cacheNames = "customerCache", key = "#id")
     public Optional<CustomerDTO> getCustomerById(UUID uuid) {
         return Optional.ofNullable(customerMapper
                 .customerToCustomerDto(customerRepository.findById(uuid).orElse(null)));
     }
 
     @Override
+    @Cacheable(cacheNames = "customerListCache")
     public List<CustomerDTO> getAllCustomers() {
         return customerRepository.findAll().stream()
                 .map(customerMapper::customerToCustomerDto)
